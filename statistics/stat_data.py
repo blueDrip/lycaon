9890e02d8ffdc3803968f7bf121e7117ea434bca
sw <1049787469@qq.com>
2016-05-24 10:26:30 +0800

    添加统计

---
+++ statistics/stat_data.py
@@ -0,0 +1,36 @@
+# coding=utf8
+import sys
+import os
+import django
+#sys.setdefaultencoding('utf8')
+if __name__ == '__main__':
+    local_dir = sys.argv[1]
+    sys.path.append(local_dir)
+    os.environ['DJANGO_SETTINGS_MODULE'] = 'lycaon.settings'
+    django.setup()
+from django.conf import settings
+from django.db.models import Q,Sum,F
+from rules.models import BaseRule
+
+from mongoengine import (
+     StringField, ListField, IntField, Document, EmbeddedDocumentField, FloatField,
+     DateTimeField, EmbeddedDocument
+)
+import logging
+import StringIO
+logger = logging.getLogger('django.rules')
+logger.setLevel(logging.INFO)
+import time
+from datetime import datetime, date, timedelta
+
+def get_time_now(hours=0,minutes=0,seconds=0):
+    today=time.localtime();
+    year=today.tm_year;
+    month=today.tm_mon;
+    day=today.tm_mday;
+    return datetime(year,month,day,hours,minutes,seconds);
+#set time
+def get_start_time(dist=1):
+    return get_time_now()-timedelta(days=dist);
+def get_stop_time(dist=1):
+    return get_time_now(23,59,59)-timedelta(days=dist);
