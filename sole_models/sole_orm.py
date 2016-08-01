2aa38b9cf32d57eba891b8581668a8bd88e72246
sw <1049787469@qq.com>
2016-08-01 18:30:14 +0800

    配置文件隐藏

--- sole_models/sole_orm.py
+++ sole_models/sole_orm.py
@@ -4,21 +4,11 @@ from django.conf import settings
 from sole_models.models import chinaUnicom_desplay,chinaMobile_desplay
 from datetime import datetime
 
-#def china_mobile_orm(cnd={}):
-#    if 'None' in cnd.values():
-#        return None
-
-#    #d=MongoDb('101.201.109.253',27017,'app_data','heigeMeixin','app_grant_data')
-#    d=MongoDb('101.201.109.253',27017,'plat_data','plat_data','plat_grant_data')
-#    c=d.get_collection('yidong').find_one(cnd,sort=[('createTime',-1)]) or {}
-
 
 def china_unicom_orm_desplay(cnd={}):
     if 'None' in cnd.values():
         return None
-    #d=MongoDb('101.201.109.253',27017,'app_data','heigeMeixin','app_grant_data')
-    d=MongoDb('101.201.109.253',27017,'plat_data','plat_data','plat_grant_data')
-    c=d.get_collection('liantong').find_one(cnd,sort=[('createTime',-1)]) or {}
+    c=settings.APPXDB.get_collection('liantong').find_one(cnd,sort=[('createTime',-1)]) or {}
     if not c:
         return None
     cubd=chinaUnicom_desplay()
@@ -35,9 +25,7 @@ def china_unicom_orm_desplay(cnd={}):
 def china_mobile_orm_desplay(cnd={}):
     if 'None' in cnd.values():
         return None
-    #d=MongoDb('101.201.109.253',27017,'app_data','heigeMeixin','app_grant_data')
-    d=MongoDb('101.201.109.253',27017,'plat_data','plat_data','plat_grant_data')
-    c=d.get_collection('yidong').find_one(cnd,sort=[('createTime',-1)]) or {}
+    c=settings.APPXDB.get_collection('yidong').find_one(cnd,sort=[('createTime',-1)]) or {}
     if not c:
         return None
     cmbd=chinaMobile_desplay()
